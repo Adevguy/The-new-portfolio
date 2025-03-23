ec2391ca03acbc3ba52f04443d207b2e0e72e3c4
Adevguy <ok5918922@gmail.com>
2025-03-23 15:42:23 +0200

    added navbar took 2 hours

---
+++ app.js
@@ -0,0 +1,5 @@
+function openMenu() {
+    const phoneNav = document.querySelector(".phone-nav")
+    const navLines = document.querySelectorAll(".nav-line")
+    phoneNav.classList.toggle("active")
+}
